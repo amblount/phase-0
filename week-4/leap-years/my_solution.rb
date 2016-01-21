0e7c6c680e88fd057c6c57f7f9387505b7ff7267
Alivia Blount <aliviablount@gmail.com>
2016-01-20 19:52:58 -0800

    my_solution leap year

--- week-4/leap-years/my_solution.rb
+++ week-4/leap-years/my_solution.rb
@@ -1,6 +1,18 @@
 # Leap Years
+  #if year % 4 == 0
+  #   if year % 100 != 0
+  #     if year % 400 == 0
+  #       return true
+  #     end
+  #   end
+  # else
+  #   return false
+  # end
 
 # I worked on this challenge [by myself, with: ].
-
+#Bethelehem
 
 # Your Solution Below
+def leap_year?(year)
+(year % 4 == 0 && year % 100 !=0) || year % 400 == 0
+end
