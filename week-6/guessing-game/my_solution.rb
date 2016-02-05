ef41bbff74af77170c451effb30d47c89676ce98
Alivia Blount <aliviablount@gmail.com>
2016-02-05 14:05:56 -0800

    Update w/ reflection 6.3 Guessing game

--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -60,14 +60,11 @@ def the_game_loop(game)
       end
     end
 
-
-
   print "Enter your guess: "
   last_guess  = gets.chomp.to_i
   last_result = game.guess(last_guess)
 
 end
-
 
 # Refactored Solution
 class GuessingGame
@@ -98,7 +95,19 @@ game = GuessingGame.new(rand(1.100))
 
 the_game_loop(game)
 
-
-
-
 # Reflection
+=begin
+How do instance variables and methods represent the characteristics and behaviors (actions) of a real-world object?
+Instance variables are able to be modified and then they are changed until they are altered again.
+
+When should you use instance variables? What do they do for you?
+You should use instance variables when you need to access a value in different methods within the same class. They allow you to keep track of the chanes you made to an instance through the call.
+
+Explain how to use flow control. Did you have any trouble using it in this challenge? If so, what did you struggle with?
+Control flow is used to map the program and what information you need to collect and manipulate before you cna move on to get new information and manipulate that.
+
+Why do you think this code requires you to return symbols? What are the benefits of using symbols?
+ Symbols are imutable and so they do not change. The point of usinf symbols is to compare the output value to the immutable to see where tha value stands in this case is it high low or correct.
+
+
+=end
