cabe7f886eaa03e9f7cc482173859c7f004011be
Alivia Blount <aliviablount@gmail.com>
2016-02-09 12:52:36 -0800

    Group Project, 7.8

--- week-7/group-project/group_project_solution.js
+++ week-7/group-project/group_project_solution.js
@@ -1,3 +1,13 @@
-// tests for sum
+//user stories
+https://developer.mozilla.org/en-US/docs/Web/API/console/assert
+//assert test
+As a user I would like to know if the given inputs are allowed for use in this function. If not print out an error statement.
 
-As a user, I want to...
+// tests for sum
+As a user, I want to make sure that sum is a function with numeric input and output. I want to determine the length of the input and classify it as an even or odd number.
+
+// tests for mean
+Mean is an equation for adding up all numbers in the array and dividing that sun by the number of numbers. The mean equations should be applied to both the even and odd arrays.
+
+// tests for median
+As a user I would like to find the median value of the even and odd data sets. The median is also the number that is halfway into the set. To find the median, the data should first be arranged in order from least to greatest. To remember the definition of a median, just think of the median of a road, which is the middlemost part of the road.
