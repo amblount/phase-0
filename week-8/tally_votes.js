25cbfe2927b63e89bd1fa8fc3c4d5c4a484401a9
Alivia Blount <aliviablount@gmail.com>
2016-02-16 16:21:41 -0800

    Create 8.3 Tally votes

--- week-8/tally_votes.js
+++ week-8/tally_votes.js
@@ -1,16 +1,38 @@
-// Tally Votes in JavaScript Pairing Challenge.
-
 // I worked on this challenge with:
 // This challenge took me [#] hours.
 
 // These are the votes cast by each student. Do not alter these objects here.
 var votes = {
-  "Alex": { president: "Bob", vicePresident: "Devin", secretary: "Gail", treasurer: "Kerry" },
-  "Bob": { president: "Mary", vicePresident: "Hermann", secretary: "Fred", treasurer: "Ivy" },
-  "Cindy": { president: "Cindy", vicePresident: "Hermann", secretary: "Bob", treasurer: "Bob" },
-  "Devin": { president: "Louise", vicePresident: "John", secretary: "Bob", treasurer: "Fred" },
-  "Ernest": { president: "Fred", vicePresident: "Hermann", secretary: "Fred", treasurer: "Ivy" },
-  "Fred": { president: "Louise", vicePresident: "Alex", secretary: "Ivy", treasurer: "Ivy" },
+  "Alex": { president: "Bob",
+           vicePresident: "Devin",
+           secretary: "Gail",
+           treasurer: "Kerry" },
+
+  "Bob": { president: "Mary",
+          vicePresident: "Hermann",
+          secretary: "Fred",
+          treasurer: "Ivy" },
+
+  "Cindy": { president: "Cindy",
+            vicePresident: "Hermann",
+            secretary: "Bob",
+            treasurer: "Bob" },
+
+  "Devin": { president: "Louise",
+            vicePresident: "John",
+            secretary: "Bob",
+            treasurer: "Fred" },
+
+  "Ernest": { president: "Fred",
+             vicePresident: "Hermann",
+             secretary: "Fred",
+             treasurer: "Ivy" },
+
+  "Fred": { president: "Louise",
+           vicePresident: "Alex",
+           secretary: "Ivy",
+           treasurer: "Ivy" },
+
   "Gail": { president: "Fred", vicePresident: "Alex", secretary: "Ivy", treasurer: "Bob" },
   "Hermann": { president: "Ivy", vicePresident: "Kerry", secretary: "Fred", treasurer: "Ivy" },
   "Ivy": { president: "Louise", vicePresident: "Hermann", secretary: "Fred", treasurer: "Gail" },
@@ -31,17 +53,91 @@ var votes = {
   "Xavier": { president: "Steve", vicePresident: "Hermann", secretary: "Fred", treasurer: "Ivy" },
   "Yvonne": { president: "Bob", vicePresident: "Zane", secretary: "Fred", treasurer: "Hermann" },
   "Zane": { president: "Louise", vicePresident: "Hermann", secretary: "Fred", treasurer: "Mary" }
-}
+};
 
-// Tally the votes in voteCount.
+//Tally the votes in voteCount.
 var voteCount = {
-  president: {},
-  vicePresident: {},
-  secretary: {},
-  treasurer: {}
-}
 
-/* The name of each student receiving a vote for an office should become a property
+
+  president: {"Bob": 0,
+             "Mary": 0,
+             "Cindy": 0,
+              "Louise":0,
+              "Fred": 0,
+              "Steve": 0,
+              "Wesley": 0,
+              "Tracy":  0,
+              "Paulina": 0,
+              "Ivy": 0,},
+
+  vicePresident: {"Devin": 0,
+                 "Hermann": 0,
+                 "John": 0,
+                 "Alex": 0,
+                 "Kerry": 0,
+                 "Mary": 0,
+                 "Oscar": 0,
+                 "Nate": 0,
+                 "Steve": 0,
+                 "Yvonne": 0,
+                 "Zane": 0,},
+
+  secretary: {"Devin": 0,
+                 "Gail": 0,
+                 "Fred": 0,
+                 "Alex": 0,
+                 "Bob": 0,
+                 "Mary": 0,
+                 "Oscar": 0,
+                 "Nate": 0,
+                 "Ivy": 0,
+                 "Valorie": 0,},
+
+  treasurer: {"Kerry": 0,
+                 "Ivy": 0,
+                 "Bob": 0,
+                 "Fred": 0,
+                 "Gail": 0,
+                 "Tracy": 0,
+                 "Xavier": 0,
+                 "Hermann": 0,
+                 "Mary": 0,},}
+
+//votes[name]["president"][electee] === votecount["president"][name] --> update value
+//votes["Bob"]["President"]
+
+for (var name in position in votes) {
+     var name = votes[president];
+       if (voteCount.president.name == "Bob") {
+         voteCount.president.Bob += 1;
+         console.log(voteCount);
+       };
+};
+
+
+var vote = (votes.Bob.president);
+voteCount.president.vote += 1;
+console.log(voteCount.president.vote)
+
+
+
+// for(var category in window.books) {
+//   if(window.books.hasOwnProperty(category)) {
+//     console.log(category); // will log "Business Books" etc.
+//     for (var i = 0, j = window.books[category].length; i < j; i++) {
+//       console.log("Title: %s, Description: %s", window.books[category][i].Title, window.books[category][i].Description);
+//     }
+//   }
+// }
+
+// // if (condition) {
+// //     block of code to be executed if the condition is true
+// // }
+// console.log(voteCount.president.Bob);
+
+
+
+ /* The name of each student receiving a vote for an office should become a property
 of the respective office in voteCount.  After Alex's votes have been tallied,
 voteCount would be ...
   var voteCount = {
@@ -55,15 +151,17 @@ voteCount would be ...
 
 /* Once the votes have been tallied, assign each officer position the name of the
 student who received the most votes. */
-var officers = {
-  president: undefined,
-  vicePresident: undefined,
-  secretary: undefined,
-  treasurer: undefined
-}
+// var officers = {
+//   president: undefined,
+//   vicePresident: undefined,
+//   secretary: undefined,
+//   treasurer: undefined
+// }
 
 // Pseudocode
+/*
 
+*/
 
 // __________________________________________
 // Initial Solution
@@ -149,3 +247,4 @@ assert(
   (officers.treasurer === "Ivy"),
   "Ivy should be elected Treasurer.",
   "8. "
+)
